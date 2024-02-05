c5f2140b4eec04c62392184013f41e676184334d
zuhridinbakhriddinov1 <zukicgleo@gmail.com>
2024-02-05 23:17:20 +0500

    Initial commit

---
+++ src/main/java/com/example/cicdtest/CicdtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdtestApplication.class, args);
+    }
+
+}
